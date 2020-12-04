bd8811c883c43851626c0387683e0282439e3c2d
nguyen thanh doi <thanhdoi.it96@hotmail.com>
2020-12-04 09:50:16 +0700

    routing

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function changeLanguage(Request $request)
+    {
+        $lang = $request->language;
+        $language = config('app.locale');
+        if ($lang == 'vi') {
+            $language = 'vi';
+        } else {
+            $language = 'en';
+        }
+        Session::put('language', $language);
+        return redirect()->back();
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return back();
+    }
+}
